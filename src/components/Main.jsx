5e123adbaa842f83d015b8387e382e082fadc7d0
marjorie <palmamarjorie410@gmail.com>
2022-09-05 16:34:29 -0600

    logo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,51 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
 import emailjs from '@emailjs/browser';
+import logo from '../assets/logo.svg'
 
 
 export const Main = () => {
 
-const sendEmail = (event) => {
-  
-  event.preventDefault();
-  emailjs.sendForm('service_l64ue24',"template_mc7bu4d",event.target,"5rYpU1zQ5DTEcG-X7")
-    .then(response => console.log(response))
+  const sendEmail = (event) => {
 
-    .catch(error=> console.log(error))
-}
-      return (
-        
+    event.preventDefault();
+    emailjs.sendForm('service_l64ue24', "template_mc7bu4d", event.target, "5rYpU1zQ5DTEcG-X7")
+      .then(response => console.log(response))
+
+      .catch(error => console.log(error))
+  }
+  return (
+
     <div className='div-form  <div class=" align-items: center">
     '>
-       <img alt="logo.svg" src="./assets/logo.svg"></img>
-       <b-card class="logo.svg-center">
-    
+      <img alt="logo.svg" src={logo} width="251" height="63" align-items-center />
+      
 
-<Formulario action=''>
-      <h1 className='title-form h3 mb-3 font-weight-normal'>Singn in</h1>
 
-        <form className='form-mail' onSubmit={sendEmail}>
-          <label></label>
-          <input type="text" placeholder='Coloque su nombre completo' name='user_name' required autofocus ></input>
-          <hr />
+        <Formulario action=''>
+          <div className='title-form h3 mb-3 font-weight-normal'>Singn up</div>
 
-          <label></label>
-          <input type="email"  placeholder='Ingrese su correo electrónico' name='user_email' required autofocus></input>
-          <hr />
 
-          <p>Recibiras un email con la contraseña autorizada por conexión</p>
-          <texarea name="user_message" id="" cols="30" rows="10"></texarea>
-          <hr />
-          <button>Send</button>
+          <div className='user'>
+          <form className='form-mail' onSubmit={sendEmail}>
+            <label></label>
+            <input type="text" placeholder='Coloque su nombre  completo' name='user_name' required autofocus ></input>
+            </div>
+            
+            <hr />
+            <div class="form-group">
+            <label></label>
+            <input type="email" placeholder='Ingrese su correo electrónico' name='user_email' required autofocus></input>
+            </div>
+            <hr />
+
+            <div class="form-group text-center">
+            <p>Recibiras un email con la contraseña<br></br> autorizada por conexión</p>
+            <texarea name="user_message" id="" cols="30" rows="10"></texarea>
+            </div>
+            <hr />
+            <div class="form-group">
+            <button class="btn btn-primary w-100" :disabled="isSubmitting">
+             <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
+               Send
+               </div>
+               </button>
+               </div>
+               </Formulario>
+             </div>
           </form>
-        </Formulario>
-        </b-card>
-        
-    </div>
+    
+  
+
+    
 
   )
 }
 
 const Formulario = styled.form`
-background: #ccc;
+background: ;
 `;
--- src/elementos/Formularios.js
+++ src/elementos/Formularios.js
@@ -15,6 +15,25 @@ const Formulario = styled.form`
         grid-template-columns: 1fr ;
         
     }
-    `; 
+    `;
+   /*  margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0; */
+    
+    const label = styled.label
+      display: block;
+      padding: 10px;
+      min-height: 40px;
+    ';
+    
+
+    const b-card{    
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin: -25px 0 0 -25px;
+}
+
+    export {Formulario};
 
     export default App
